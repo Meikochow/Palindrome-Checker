67a2a2f23d2b6270bee717612f30c5809979b646
Victor Cojocaru <39445275+Meikochow@users.noreply.github.com>
2018-08-17 12:11:21 -0500

    Update palindromeChecker.js

--- palindromeChecker.js
+++ palindromeChecker.js
@@ -1,17 +1,49 @@
+//V1
 function palindrome(str) {
-let regex = /[_\W]/g;
-let newStr = str.replace(regex,'').toLowerCase().split('');
-let checker = true;
-    for (var i=0;i<newStr.length/2;i++){
-      if(newStr[i] !== newStr[newStr.length-i-1]){
-      checker = false;
+let regex = /[\W_]/g;
+str = str.replace(regex,'').toLowerCase();
+    for (var i=0;i<str.length/2;i++){
+      if(str[i] !== str[str.length-i-1]){
+      console.log(str, " :is not a palindrome");
+      return false;
       }
     }
-    console.log(str, " :is a palindrome",checker)
-    return checker; 
+    console.log(str, " :is a palindrome");
+    return true; 
 }
-   console.clear();
-palindrome("_Eyye");
+console.clear();
+palindrome("_Eyy");
+palindrome('not a palindrome');
+palindrome("race car");
+palindrome("five|\_/|four");
+palindrome("0_0 (: /-\ :) 0-0");
+palindrome("My age is 0, 0 si ega ym.");
+palindrome("A man, a plan, a canal. Panama");
+//V2
+function palindrome(str) {
+let start = 0;
+let finish = str.length-1;
+while (finish>start){
+    if(str[start].match(/[\W_]/)){
+        start++;
+        continue
+    }
+    if(str[finish].match(/[\W_]/)){
+        finish--;
+        continue
+    }
+    if(str[start].toLowerCase()!==str[finish].toLowerCase()){
+        console.log(str," : is not a Palindrome;");
+        return false;
+    }
+    start++;
+    finish--;
+}
+console.log(str," : is a Palindrome;");
+return true;
+}
+console.clear();
+palindrome("_Eyy");
 palindrome('not a palindrome');
 palindrome("race car");
 palindrome("five|\_/|four");
